0e8e82ae8bc48bf01fc59b4c843d7f76d824c918
Halit Dincer <github@halitdincer.com>
2024-01-17 18:03:56 -0500

    customize the starter little bit and remove unnecessary stuff

--- server/app.js
+++ server/app.js
@@ -7,18 +7,3 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("Server started"));
 
 app.use(express.static("build"));
-
-const items = [
-  {
-    name: "Laptop",
-    price: 500,
-  },
-  {
-    name: "Desktop",
-    price: 700,
-  },
-];
-
-app.get("/api/items", (req, res) => {
-  res.send(items);
-});
